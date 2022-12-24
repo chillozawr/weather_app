e84d05b06e9286dba14c8850637f536065fc6e92
chillozawr <77758652+chillozawr@users.noreply.github.com>
2022-12-24 18:47:39 +0400

    Update Hourly.tsx

--- src/pages/day/Hourly.tsx
+++ src/pages/day/Hourly.tsx
@@ -2,12 +2,16 @@ import React, { FC } from 'react';
 import styles from './Hourly.module.scss';
 import Today from '../../components/Today';
 import OtherDays from '../../components/OtherDays';
+import SearchModal from '../../components/modals/SearchModal';
+import { useAppSelector } from '../../hooks';
 
 const Hourly: FC = () => {
+  const { showModal } = useAppSelector((state) => state.weather);
   return (
     <div className={styles.container}>
       <Today />
       <OtherDays />
+      {showModal && <SearchModal />}
     </div>
   );
 };
